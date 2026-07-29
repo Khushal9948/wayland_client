a8d2028766c30734436c2f0247e915085f6b8988
khushal9948 <khushalpatel9948@gmail.com>
2026-07-29 17:34:39 +0530

    ...

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <wayland-client.h>
 
-int main(void) {
-  struct wl_display *display;
-
-  display = wl_display_connect(NULL);
-
-  if (display == NULL) {
-    fprintf(stderr, "Failed to connect to Wayland compositor.\n");
+int main(int argc, char *argv[]) {
+  struct wl_display *display =
+      wl_display_connect("wayland-0"); // NULL or wayland-0
+  if (!display) {
+    fprintf(stderr, "Failed to connect to Wayland display.\n");
     return 1;
   }
-
-  printf("Connected successfully!\n");
+  fprintf(stderr, "Connection established!\n");
 
   wl_display_disconnect(display);
-
   return 0;
 }
